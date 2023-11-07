23d82da532ced1484b5eb75691b6c27056fc7ca4
pyscripter99 <ryder@retzlaff.family>
2023-11-07 14:40:25 +1100

    Minimal server

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("[%s] %s %s", r.Method, r.URL, r.RemoteAddr)
+		w.Write([]byte("Hello World!"))
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Println("Server closed")
+	} else if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+		os.Exit(0)
+	}
 }
