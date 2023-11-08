23c46ea1acfafd83ece6fb4b32d1e2d1da25d43e
pyscripter99 <ryder@retzlaff.family>
2023-11-08 14:59:42 +1100

    Fix missing quote in session struct

--- api/types/session.go
+++ api/types/session.go
@@ -3,7 +3,7 @@ package types
 import "time"
 
 type Session struct {
-	ID          int       `json:"id`
+	ID          int       `json:"id"`
 	DownloadURL string    `json:"download_url"`
 	Expiration  time.Time `json:"expiration"`
 }
