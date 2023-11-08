8a01651aeba9896180d1f164a4677ee6f4bdffa0
pyscripter99 <ryder@retzlaff.family>
2023-11-08 14:34:11 +1100

    Add handlers files

--- main.go
+++ main.go
@@ -13,13 +13,6 @@ import (
 //go:embed web/*
 var web embed.FS
 
-func loggerMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("[%s] %s %s", r.Method, r.URL, r.RemoteAddr)
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	mux := http.NewServeMux()
 
